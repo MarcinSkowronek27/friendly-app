49e3516b7e8984cb381795bfff05a3d977cf341d
Marcin <marcin.skowronek303@gmail.com>
2022-09-02 14:31:04 +0200

    delete unnecessary files

--- src/app/models/interfaces/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './work-request-dto.interface';
-export * from './work-response-dto.interface';
--- src/app/models/interfaces/work-response-dto.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface WorkResponseDto {
-  work_order_id: number;
-  description: string;
-  received_date: string;
-  assigned_to: [object];
-  status: string;
-  priority: string;
-}
